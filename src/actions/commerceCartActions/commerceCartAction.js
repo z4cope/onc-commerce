4dd94f24c249a82b330da69b44871c47253bdf31
z4cope <63551733+z4cope@users.noreply.github.com>
2021-12-15 13:00:44 +0200

    deleted action

--- src/actions/commerceCartActions/commerceCartAction.js
+++ src/actions/commerceCartActions/commerceCartAction.js
@@ -3,7 +3,7 @@ import { commerce } from "../../lib/commerce";
 
 export const commerceCartAction = (productId, quantity) => async (dispatch) => {
   const cartData = await commerce.cart.retrieve();
-  const itemData = await commerce.cart.add(productId, quantity);
+
   dispatch({
     type: FETCH_COMMERCE_CART,
     payload: { cartList: cartData, itemList: itemData },
