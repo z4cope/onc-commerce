e5e421aec68ffc25dbbcf510ce692c7133fd341f
z4cope <63551733+z4cope@users.noreply.github.com>
2022-03-24 21:21:05 +0200

    init

--- server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, "Mm0122629004", {
-    expiresIn: "30d",
-  });
-};
-
-module.exports = generateToken;
